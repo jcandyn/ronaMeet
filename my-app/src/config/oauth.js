9480401fe44a5dacaffe0b49b3cb23ce25aae99b
jcandyn <50030017+jcandyn@users.noreply.github.com>
2020-11-15 02:50:17 -0500

    switched login in project to mongoDB

---
+++ my-app/src/config/oauth.js
@@ -0,0 +1,8 @@
+module.exports = {
+    facebookAuth: {
+       clientID: "366742914415784",
+       clientSecret: "30f1325e64b307db02908c26130266ae",
+       callbackURL: "http://localhost:3000/auth/facebook/callback",
+       enableProof: false
+      }
+    }
